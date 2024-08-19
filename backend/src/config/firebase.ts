1e130d3c2c3432d2b861da684ae3eec958ffc848
Maryna Kucher <maryna.dev.k@gmail.com>
2024-08-19 14:55:41 +0300

    firebase configuretion are added

---
+++ backend/src/config/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { envFirebase } from 'config';
+
+const firebaseConfig = {
+  apiKey: envFirebase.apiKey,
+  authDomain: envFirebase.authDomain,
+  projectId: envFirebase.projectId,
+  storageBucket: envFirebase.storageBucket,
+  messagingSenderId: envFirebase.messagingSenderId,
+  appId: envFirebase.appId,
+};
+
+const app = initializeApp(firebaseConfig);
+const dbFirestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, dbFirestore, auth };
